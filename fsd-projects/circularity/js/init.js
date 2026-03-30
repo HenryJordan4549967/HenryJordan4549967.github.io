b21f63fb1e8e7f0fbd8d67f7b300c2650fe601f9
HenryJordan4549967 <hj4549967@lpssonline.com>
2026-03-30 14:08:50 +0000

    todo 4 complete

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -20,7 +20,7 @@ var init = function (window) {
         ///////////////////
         
         // TODO 1 : Declare and initialize our variables
-var circle = []
+var circles = []
 
 
         // TODO 2 : Create a function that draws a circle 
@@ -60,7 +60,15 @@ drawCircle()
         */
         function update() {
             // TODO 4 : Update the position of each circle using physikz.updatePosition()
+physikz.updatePosition(circles[ 0 ])
 
+physikz.updatePosition(circles[ 1 ])
+
+physikz.updatePosition(circles[ 2 ])
+
+physikz.updatePosition(circles[ 3 ])
+
+physikz.updatePosition(circles[ 4 ])
             
             // TODO 5 : Call game.checkCirclePosition() on your circles
            
